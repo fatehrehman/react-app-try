1f09c79249b1588da5e4b9604a10f7bc11e670f0
Fateh Rehman <fatehrehman@apples-mbp.mynetworksettings.com>
2023-01-29 18:14:22 -0500

    Chapter 2-2

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import "./index.css";
 //import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const elementX = <h1>Hi</h1>;
+function clock() {
+  const elementX = (
+    <div>
+      <h1>Hello</h1>
+      <h2>Tick {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  root.render(elementX);
+}
+setInterval(clock, 1000);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elementX);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
