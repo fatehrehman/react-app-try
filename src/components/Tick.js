19b0afcd619a77a2d0bf85e91f082bb6a9e27f6c
Fateh Rehman <fatehrehman@apples-mbp.mynetworksettings.com>
2023-01-29 20:43:14 -0500

    Chapter 3-7

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 function Tick() {
   const [ticker, setTikcer] = useState(new Date().toLocaleTimeString());
   // [varName (like name), fuctionName (like setName) to set variable name]
+  const [varX, setVarX] = useState("X");
+
   function showTime() {
     setTikcer(new Date().toLocaleTimeString());
+    setVarX("Y");
   }
 
   useEffect(() => {
-    console.log("This is called on load and on each state change");
-  });
+    console.log("This is called on load and on each varx state change");
+  }, [varX]);
 
   setInterval(showTime, 1000);
+
   return <h1>{ticker}</h1>;
 }
 export default Tick;
