d47d8e8776034d982d2468cc3a6a31b02da27c61
Fateh Rehman <fatehrehman@apples-mbp.mynetworksettings.com>
2023-01-29 20:52:48 -0500

    Chapter 3-8

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from "react";
 function Tick() {
   const [ticker, setTikcer] = useState(new Date().toLocaleTimeString());
   // [varName (like name), fuctionName (like setName) to set variable name]
-  const [varX, setVarX] = useState("X");
+  //   const [varX, setVarX] = useState("X");
 
   function showTime() {
     setTikcer(new Date().toLocaleTimeString());
-    setVarX("Y");
+    // setVarX("Y");
   }
 
   useEffect(() => {
-    console.log("This is called on load and on each varx state change");
-  }, [varX]);
-
-  setInterval(showTime, 1000);
+    // UseEffect is used to do some api call or to perform other action on some or specific var's state change
+    console.log("This is called on load & ticker change");
+    const tickercall = setInterval(showTime, 1000);
+    return () => {
+      console.log("This is called at the end of UseEffect");
+      clearInterval(tickercall);
+      // it is used for cleanup like hide loader etc or to do other action at the end of UseEffect
+    };
+  }, [ticker]);
 
   return <h1>{ticker}</h1>;
 }
