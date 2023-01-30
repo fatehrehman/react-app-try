1ccc0c410fc0682b3a9c6e4b3e0839ee8418c268
Fateh Rehman <fatehrehman@apples-mbp.mynetworksettings.com>
2023-01-29 19:31:08 -0500

    Chapter 3-5

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 function Tick() {
+  const [ticker, setTikcer] = useState(new Date().toLocaleTimeString());
+  // [varName (like name), fuctionName (like setName) to set variable name]
   function showTime() {
-    // Tick clock is not working because we have not used state property in component
-    return <h1>Tick {new Date().toLocaleTimeString()}</h1>;
+    setTikcer(new Date().toLocaleTimeString());
   }
   setInterval(showTime, 1000);
-  return <div>{showTime()}</div>;
+  return <h1>{ticker}</h1>;
 }
 export default Tick;
