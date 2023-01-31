ddae991c1ea51ad4c4c801f5e2720aae0883c7c1
Fateh Rehman <fatehrehman@apples-mbp.mynetworksettings.com>
2023-01-30 20:18:33 -0500

    Chapter 6-2a

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,6 @@
 function Blog(props) {
   const content = props.posts.map((post) => {
-    return <Post post={post} />;
+    return <Post key={post.id} post={post} />;
   });
   return <div>{content}</div>;
 }
@@ -8,7 +8,8 @@ function Blog(props) {
 function Post(props) {
   const { id, title, content } = props.post;
   return (
-    <div key={id}>
+    // <div key={id}> // it will show console error as id is required for post component
+    <div>
       <div>ID: {id}</div>
       <div>Title: {title}</div>
       <div>Content: {content}</div>
